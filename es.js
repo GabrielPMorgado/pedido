375b8cf1cbac572605d43ccfbcb1e936192ab834
Gabriel <141271554+GabrielPMorgado@users.noreply.github.com>
2024-09-09 17:36:04 -0300

    add

---
+++ es.js
@@ -0,0 +1,7 @@
+document.getElementById("yesBtn").addEventListener("click", function() {
+    document.getElementById("responseMessage").textContent = "Estou muito feliz! Mal posso esperar para estar com você no sábado. 💖";
+});
+
+document.getElementById("noBtn").addEventListener("click", function() {
+    document.getElementById("responseMessage").textContent = "Entendo, quem sabe em outra ocasião... 😔 Mas continuo pensando em você.";
+});
